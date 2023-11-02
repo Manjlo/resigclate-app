951a274c60d8105946b371f394971aa3d03f1240
jenestiven <jhon.aricapa@correounivalle.edu.co>
2023-11-02 16:11:43 -0500

    organnice component RouteHandler

--- src/components/screens/Register.jsx
+++ src/components/screens/Register.jsx
@@ -1,8 +1,26 @@
+import { LinkToCreateAccount } from "../atoms/LinkToCreateAccount"
+import { LoginButton } from "../atoms/LoginButton"
+import { InfoItem } from "../molecules/InfoItem"
 
-function Register () {
+function Register() {
   return(
-    <h1>hola</h1>
+    <div>
+       <h2 className='text-xl sm:text-2xl sm:mr-8'>Crea tu cuenta</h2>
+      <ul>
+        <li>
+          <InfoItem text={'Tu nombre'}/>
+          <InfoItem text={'Tu correo usuario'}/>
+          <InfoItem type={'password'} text={'Tu contraseña'}/>
+          <InfoItem text={'Tu celular'}/>
+        </li>
+      </ul>
+      <LoginButton textButton={'Crea tu cuenta'}/>
+      <div className='relative'>
+        <div className='absolute inset-0 border-t-2 border-[#37373e55] opacity-50 h-2 sm:w-0'></div>
+        <LinkToCreateAccount/>
+      </div> 
+    </div>
   )
 }
 
-export { Register }
+export { Register };
