bea52b5618ea0d2c288f5ca6d8dfd71715c0cd93
Jhonatan Castaño <80026675+Manjlo@users.noreply.github.com>
2023-11-15 18:59:32 -0500

    add firebase hosting and automatic deploy with github actions

--- src/components/templates/MyMap.jsx
+++ src/components/templates/MyMap.jsx
@@ -1,40 +1,62 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import { LayerSwitcher } from '../molecules/LayerSwitcher';
-import { MyCustomControl } from '../organims/CustomControl';
-import { SearchItem } from '../molecules/SearchItem';
-import { ReactComponent as UserSvg } from '../../assets/svg/searchsvg.svg'
-import { ReactComponent as iconBarSvg } from '../../assets/svg/iconBars.svg'
-import Logo from '../../assets/icons/logo.png'
-import { LocationMarker } from '../atoms/LocationMarker';
-import { ItemsBar } from '../molecules/ItemsBar';
-import usuarioUrl from '../../assets/icons/perfil.png'
-import { SvgToDiv } from '../atoms/SvgToDiv';
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { LayerSwitcher } from "../molecules/LayerSwitcher";
+import { MyCustomControl } from "../organims/CustomControl";
+import { SearchItem } from "../molecules/SearchItem";
+import { ReactComponent as UserSvg } from "../../assets/svg/searchsvg.svg";
+import { ReactComponent as iconBarSvg } from "../../assets/svg/iconBars.svg";
+import Logo from "../../assets/icons/logo.png";
+import { LocationMarker } from "../atoms/LocationMarker";
+import { ItemsBar } from "../molecules/ItemsBar";
+import usuarioUrl from "../../assets/icons/perfil.png";
+import { SvgToDiv } from "../atoms/SvgToDiv";
 
-
-const usuario = 'javier';
+const usuario = "javier";
 
 const MyMap = ({ center, zoom, scrollWheelZoom, baseLayers, zoomControl }) => {
   return (
-    <MapContainer className='sm:rounded-md' center={center} zoom={zoom} scrollWheelZoom={scrollWheelZoom} zoomControl={zoomControl}>
-      <MyCustomControl>
-        <TileLayer attribution={baseLayers[0].attribution} url={baseLayers[0].url} />
-        <MyCustomControl className={'h-[70px] w-[70px] right-[88vw] relative rounded-full shadow-sm'}>
-          <img src={Logo} alt="Logo" />
+    <div className="h-screen w-screen">
+      <MapContainer
+        className="sm:rounded-md"
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={scrollWheelZoom}
+        zoomControl={zoomControl}
+      >
+        <MyCustomControl>
+          <TileLayer
+            attribution={baseLayers[0].attribution}
+            url={baseLayers[0].url}
+          />
+          <MyCustomControl
+            className={
+              "h-[70px] w-[70px] right-[88vw] relative rounded-full shadow-sm"
+            }
+          >
+            <img src={Logo} alt="Logo" />
+          </MyCustomControl>
+          <MyCustomControl
+            className={
+              "w-[300px] lg:w-[400px] right-[33vw] top-[-60px] h-auto bg-white relative rounded-md shadow-lg text-md"
+            }
+          >
+            <SearchItem
+              type={"search"}
+              text={"Buscar centros de reciclaje"}
+              SvgComponent={UserSvg}
+            />
+          </MyCustomControl>
+          <MyCustomControl className="grid grid-rows-2 gap-2 sm:w-10 sm:h-24 bg-white shadow-md rounded-md relative top-[44px]">
+            <LocationMarker />
+          </MyCustomControl>
+          <LayerSwitcher baseLayers={baseLayers} />
         </MyCustomControl>
-        <MyCustomControl className={'w-[300px] lg:w-[400px] right-[33vw] top-[-60px] h-auto bg-white relative rounded-md shadow-lg text-md'}>
-          <SearchItem type={'search'} text={'Buscar centros de reciclaje'} SvgComponent={UserSvg} />
-        </MyCustomControl>
-        <MyCustomControl className='grid grid-rows-2 gap-2 sm:w-10 sm:h-24 bg-white shadow-md rounded-md relative top-[44px]'>
-          <LocationMarker />
-        </MyCustomControl>
-        <LayerSwitcher baseLayers={baseLayers} />
-        <MyCustomControl className={'w-auto h-0 bg-red-200 absolute z-20 right-[29vw] top-[140px]'}>
-          <SvgToDiv SvgComponent={iconBarSvg} />
-          <ItemsBar perfilurl={usuarioUrl} usuario={usuario} />
-        </MyCustomControl>
-      </MyCustomControl>
-    </MapContainer>
+      </MapContainer>
+      <div className="navbar-controller absolute bottom-9 right-1/3 z-[1000]">
+        <SvgToDiv SvgComponent={iconBarSvg} />
+        <ItemsBar perfilurl={usuarioUrl} usuario={usuario} />
+      </div>
+    </div>
   );
 };
 
---
+++ src/services-controller/services.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { getToken } from "../services-firebase/auth";
+
+const PATH_BASE = "https://us-central1-justo-pago.cloudfunctions.net/app/";
+//const PATH_BASE = "http://127.0.0.1:5001/justo-pago/us-central1/app/";
+const configHeaders = async () => {
+  const token = await getToken();
+  return {
+    headers: {
+      Authorization: `${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  };
+};
+
+const get = async (path) => {
+  const config = await configHeaders();
+  const response = await axios.get(`${PATH_BASE}${path}`, config);
+  return response.data;
+};
+
+const post = async (path, object) => {
+  const config = await configHeaders();
+  const response = await axios.post(`${PATH_BASE}${path}`, object, config);
+  return response.data;
+};
+
+const put = async (path, object) => {
+  const config = await configHeaders();
+  const response = await axios.put(`${PATH_BASE}${path}`, object, config);
+  return response.data;
+};
+
+const remove = async (path) => {
+  const config = await configHeaders();
+  const response = await axios.delete(`${PATH_BASE}${path}`, config);
+  return response.data;
+};
+
+export { get, post, put, remove };
